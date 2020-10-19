eda1d25a240bdf42a41af3ebaf59fbe022115d81
Zhou Wang <wangzhou1@hisilicon.com>
2020-10-19 11:59:48 +0800

    ...
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

--- c_note/printf_n.c
+++ c_note/printf_n.c
@@ -8,11 +8,13 @@ int main()
 	printf("%*c %d\n", num, ' ', 123);
 	printf("%*c %d\n", num, ' ', 456);
 
-	return 0;
+	return 3;
 }
 
+#if 0
 输出：
 xxx@kllp05:~/tests/c_note$ ./a.out 
 123456:
         123
         456
+#endif
