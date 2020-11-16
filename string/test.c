07c08614dea69a780221a139e1b3b9634bd4bc92
Zhou Wang <wangzhou1@hisilicon.com>
2020-11-16 06:30:36 +0000

    ..
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

--- string/test.c
+++ string/test.c
@@ -1,13 +1,23 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
 	char *s = "12345";
 	char *t;
+	char t1[] = {'1', '2', ' ', '3', '4', ' ', '5', '6', '\0'};
+	char *str = t1;
+	char *tmp;
 
 	t = rindex(s, '3');
 	printf("%s\n", t);
+
+	while (tmp = strtok(str, " ")) {
+		printf("%s\n", tmp);
+		printf("---> %ld\n", strtol(tmp, NULL, 0));
+		str = NULL;
+	}
 
 	return 0;
 }
