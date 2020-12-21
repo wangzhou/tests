95ce189faf4887b9d1fabfbb0fb055ecdfe0421c
Zhou Wang <wangzhou1@hisilicon.com>
2020-12-21 10:45:01 +0000

    ...
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>

---
+++ fork/fork.c
@@ -0,0 +1,21 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int count = 1;
+
+int main()
+{
+	pid_t pid;
+
+	if ((pid = fork()) < 0) {
+		printf("fail to fork\n");
+	} else if (pid == 0) {
+		printf("in child process %d\n", count);
+	} else {
+		printf("in father process %d\n", count);
+	}
+}
