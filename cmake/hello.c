176daa1c970df1f7558b401d86619e34734f46c5
Zhou Wang <wangzhou89@126.com>
2021-11-29 23:44:35 +0800

    ...
    Signed-off-by: Zhou Wang <wangzhou89@126.com>

--- cmake/hello.c
+++ cmake/hello.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <math.h>
+#include <numa.h>
 #include "hello.h"
 #include "./lib/add.h"
 
 int main()
 {
+	int max = 5;
+
 	printf(HELLO);
 	printf("1 + 2 = %d\n", add(1, 2));
+	printf("sqrt(5) = %f\n", sqrt(5));
+	max = numa_max_node();
+	printf("max node = %d\n", max);
 
 	return 0;
 }
